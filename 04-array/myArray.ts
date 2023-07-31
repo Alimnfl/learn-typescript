401ab669262fad5fe134dec98e1313977ff64cf1
Alimnfl <mnaufallim234@gmail.com>
2023-08-01 00:43:08 +0700

    Added Array

---
+++ 04-array/myArray.ts
@@ -0,0 +1,24 @@
+// 11
+
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+// N - type from User
+const allUsers: User[] = [];
+
+// N - for number, have 2 array number
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [300, 250, 150],
+];
+
+superHeroes.push('spiderman');
+heroPower.push(3);
+
+allUsers.push({ name: '', isActive: true });
