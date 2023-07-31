1bb69e2df3e631b5144889111ebed63499b102fe
Alimnfl <mnaufallim234@gmail.com>
2023-08-01 01:44:09 +0700

    Added Array JS for TS

---
+++ 04-array/myArray.js
@@ -0,0 +1,14 @@
+// 11
+var superHeroes = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+// N - type from User
+var allUsers = [];
+// N - for number, have 2 array number
+var MLModels = [
+    [255, 255, 255],
+    [300, 250, 150],
+];
+superHeroes.push('spiderman');
+heroPower.push(3);
+allUsers.push({ name: '', isActive: true });
